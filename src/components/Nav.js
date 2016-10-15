130aa40e648f3a691b61c900b9f3e3b69b76fa5e
mjstromberg <michaeljstromberg@gmail.com>
2016-10-14 21:49:39 -0700

    Implement basic Nav function component

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+var Nav = () => (
+  <div className="navbar">
+    <div className="col-md-2">Home</div>
+    <div className="col-md-2">Players</div>
+    <div className="col-md-8">
+      <Search />
+    </div>
+  </div>
+);
+
+window.Nav = Nav;
